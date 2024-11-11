a68b52b7aeab793ace6a02dd92626833286d20d1
DO <thuhoaivo13324@gmail.com>
2024-11-11 16:24:01 +0700

    Serializers

---
+++ backend/api/serializer.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+
+from api import models as api_models
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        token['username'] = user.username
+        return token
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
